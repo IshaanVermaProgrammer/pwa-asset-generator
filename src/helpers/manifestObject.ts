0957b1ce371c95cb98190d7c9065353543aecb8a
IshaanVermaProgrammer <67543939+IshaanVermaProgrammer@users.noreply.github.com>
2021-10-08 17:09:42 +0530

    Create manifestObject.ts

---
+++ src/helpers/manifestObject.ts
@@ -0,0 +1,10 @@
+export const manifestObject={
+    "name":"Progressive Web App",
+    "short_name":"PWA",
+    "start_url":".",
+    "display":"standalone",
+    "background_color":"#FFE9D2",
+    "theme_color":"#FFE1C4",
+    "orientation":"potrait-primary",
+    "icons":[]
+}
